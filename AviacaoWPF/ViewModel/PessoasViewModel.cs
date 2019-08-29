a2d38a95bbb71cfb32da21810e2edb7c430b0bd3
1827524 <1827524@DASO113959>
2019-08-28 21:54:33 -0300

    Adicionado

---
+++ AviacaoWPF/ViewModel/PessoasViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Aviacao;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviacaoWPF.ViewModel
+{
+    public class PessoasViewModel
+    {
+
+        public ObservableCollection<Pessoa> Pessoas { get; set; }
+
+        public Boolean PodeRemover
+        {
+            get
+            {
+                return this.PessoaSelecionada != null;
+            }
+        }
+
+        public Pessoa PessoaSelecionada { get; set; }
+
+        public ModelAviacao modelAviacao = new ModelAviacao();
+
+        public PessoasViewModel()
+        {
+            this.Pessoas = new ObservableCollection<Pessoa>(modelAviacao.Pessoas.ToList());
+            this.PessoaSelecionada = modelAviacao.Pessoas.FirstOrDefault();
+        }
+
+        public void Salvar()
+        {
+            this.modelAviacao.SaveChanges();
+        }
+
+        public void Remover()
+        {
+            if (this.PessoaSelecionada.Id != 0)
+            {
+                this.Pessoas.Remove(PessoaSelecionada);
+                this.modelAviacao.Pessoas.Remove(PessoaSelecionada);
+            }
+        }
+
+        public void Adicionar()
+        {
+            Pessoa p = new Pessoa();
+            this.Pessoas.Add(p);
+            this.PessoaSelecionada = p;
+            this.modelAviacao.Pessoas.Add(p);
+        }
+    }
+}
---
+++ AviacaoWPF/WindowPessoa.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Aviacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AviacaoWPF
+{
+    /// <summary>
+    /// Lógica interna para WindowPessoa.xaml
+    /// </summary>
+    public partial class WindowPessoa : Window
+    {
+        public ViewModel.PessoasViewModel PessoasViewModel { get; set; }
+        public WindowPessoa()
+        {
+            InitializeComponent();
+            this.PessoasViewModel = new ViewModel.PessoasViewModel();
+            this.DataContext = this;
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.PessoasViewModel.Salvar();
+            this.Close();
+        }
+
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            PessoasViewModel.Adicionar();
+        }
+
+        private void btnRemover_Click(object sender, RoutedEventArgs e)
+        {
+            PessoasViewModel.Remover();
+        }
+
+        private void ListView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
